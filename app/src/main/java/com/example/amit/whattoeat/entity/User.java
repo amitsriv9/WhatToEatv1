6d9b2a6b69cde7a3097c507d274899600e6017c1
Amit Srivastava <amitsriv9@gmail.com>
2015-05-02 18:57:06 -0400

    user details

--- app/src/main/java/com/example/amit/whattoeat/entity/User.java
+++ app/src/main/java/com/example/amit/whattoeat/entity/User.java
@@ -4,4 +4,16 @@ package com.example.amit.whattoeat.entity;
  * Created by Amit on 4/15/2015.
  */
 public class User {
+    // user info to persist here for storage
+
+    int age;
+    Long  weight;
+    Long  height_feet, height_inches;
+    String gender;
+
+    private User(){}
+
+    long claculate_my_bmi(){return (weight/(12*height_feet + height_feet)^2) * 703;}
+
+
 }
